4763e0ad313ab71bf9b90ad3e4d669a960c2e415
Vinicius Sousa <viniciusps.1190@gmail.com>
2024-11-08 15:02:07 -0300

    Adicionando comentário para conexão ao postgreSQL

--- src/main/java/br/com/uam/basefacts/facts/FactsController.java
+++ src/main/java/br/com/uam/basefacts/facts/FactsController.java
@@ -23,4 +23,19 @@ public class FactsController {
     public List<Facts> getFacts() {
         return factsService.getFacts();
     }
+
+    /*
+    Mano, pra puxar esse caba, tu tem que iniciar o seu postgres no usuário postgres e com a senha que tu colocou.
+    Depois, tu tem que criar um banco de dados chamado "facts" e um usuário chamado "facts" com a senha "facts".
+    da pra fazer, por terminal. Se tu tiver instalado certinho o postgres e o bin dele estiver nas variáveis de ambiente,
+    dá um:
+
+    $> psql -U postgres
+    $> \c facts
+
+    Isso vai te conectar ao banco de dados "facts" com o usuário "postgres".:
+     */
+
+    // Se não der certo, dá um salve no zap que a gente tenta fazer essa bagaça ai funcionar no teu pc tbm.
+
 }
--- src/main/java/br/com/uam/basefacts/facts/FactsRepository.java
+++ src/main/java/br/com/uam/basefacts/facts/FactsRepository.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 
 public interface FactsRepository extends JpaRepository<Facts, Long> {
 
-    @Query("SELECT f FROM Facts f WHERE f.userEmail = ?1") // ?1 is a placeholder for the first parameter.
-    Optional<Facts> findByEmail(String email);
+    @Query("SELECT f FROM Facts f WHERE f.userEmail = ?1") // '?1' is a placeholder for the first parameter.
+    Optional<Facts> findByEmail(String email); // String email is the parameter that we talked about in the query.
+                                                    // How spring boot deals with it, I don't know.
+    // Optional is a container object which may or may not contain a non-null value.
 
 }
