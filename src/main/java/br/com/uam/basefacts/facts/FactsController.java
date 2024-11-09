e44faaab694c8783be235eea7350aa8575743347
Vinicius Sousa <viniciusps.1190@gmail.com>
2024-11-08 22:41:46 -0300

    Subindo com um bug no @Putmapping, por algum motivo ele não altera o email do usuário. Tenho que ver isso depois

--- src/main/java/br/com/uam/basefacts/facts/FactsController.java
+++ src/main/java/br/com/uam/basefacts/facts/FactsController.java
@@ -1,9 +1,7 @@
 package br.com.uam.basefacts.facts;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,18 +22,23 @@ public class FactsController {
         return factsService.getFacts();
     }
 
-    /*
-    Mano, pra puxar esse caba, tu tem que iniciar o seu postgres no usuário postgres e com a senha que tu colocou.
-    Depois, tu tem que criar um banco de dados chamado "facts" e um usuário chamado "facts" com a senha "facts".
-    da pra fazer, por terminal. Se tu tiver instalado certinho o postgres e o bin dele estiver nas variáveis de ambiente,
-    dá um:
+    @PostMapping
+    public void SubmitFacts(@RequestBody Facts facts){
+        factsService.addNewFacts(facts);
+    }
 
-    $> psql -U postgres
-    $> \c facts
+    @DeleteMapping(path="{factsId}")
+    public void deleteFacts(
+            @PathVariable("factsId") Long factsId) {
+        factsService.deleteFacts(factsId);
+    }
 
-    Isso vai te conectar ao banco de dados "facts" com o usuário "postgres".:
-     */
-
-    // Se não der certo, dá um salve no zap que a gente tenta fazer essa bagaça ai funcionar no teu pc tbm.
+    @PutMapping(path = "{factsId}")
+    public void updateFacts(
+            @PathVariable("factsId") Long factsId,
+            @RequestParam(required = false) String url,
+            @RequestParam(required = false) String email){
+        factsService.updateFacts(factsId, url, email);
+    }
 
 }
